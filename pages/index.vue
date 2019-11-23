17067704da52d034e111d2a53a58c08b669241e7
Muhar Dwi Purwanto <k@muhar.us>
2019-11-23 18:26:07 +0700

    CHANGE: temporary main content

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        ka.muhar.us
-      </h1>
-      <h2 class="subtitle">
-        Kamu Harus Apa?
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    Kamu Harus
   </div>
 </template>
 
